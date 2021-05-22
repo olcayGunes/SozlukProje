fd25ed6ddd603bee6d3d993e4f082d590750ade7
olcayGunes <65913895+olcayGunes@users.noreply.github.com>
2021-05-22 21:41:03 +0300

    44. Ders Sonu

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+	public class WriterValidator:AbstractValidator<Writer>
+	{
+		public WriterValidator()
+		{
+			RuleFor(c => c.WriterName).NotEmpty().WithMessage("Yazar Adı Boş Geçilemez.");
+			RuleFor(c => c.WriterSurName).NotEmpty().WithMessage("Yazar Soyadı Boş Geçilemez.");
+			RuleFor(c => c.WriterAbout).NotEmpty().WithMessage("Hakkında Bölümü Boş Geçilemez.");
+			RuleFor(c => c.WriterName).MinimumLength(2).WithMessage("Yazar Adı En Az 2 Karakter Olmalıdır.");
+			RuleFor(c => c.WriterSurName).MinimumLength(2).WithMessage("Yazar Soyadı En Az 2 Karakter Olmalıdır.");
+		}
+	}
+}
